e765c0699bda80602fb935b2a2cde1daeed1ce4a
Thamar4 <d2023012267@unifei.edu.br>
2025-07-01 00:13:56 -0300

    Tela de Cadastro

---
+++ salas/src/pages/Cadastro.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import styles from "./Cadastro.module.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Cadastro() {
+  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleContact = () => {
+    console.log("Enviado!");
+    return navigate("/LoginForm");
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <header className={styles.mainCabeçalho}>
+        <h1>Sistema Reserva de Salas</h1>
+      </header>
+      <div className={styles.box}>
+        <h3>Cadastre-se</h3>
+        <div className={styles.campoTextual}>
+          <input
+            type="text"
+            placeholder="Nome"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className={styles.campoTextual}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className={styles.campoTextual}>
+          <input
+            type="password"
+            placeholder="Senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className={styles.button}>
+          <button type="submit">Cadastrar</button>
+        </div>
+
+        <div className={styles.button}>
+          <button onClick={handleContact}>Voltar ao Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
